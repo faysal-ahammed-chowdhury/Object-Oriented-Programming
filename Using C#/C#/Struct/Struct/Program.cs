2de2cb413934133f3ffe818d2984f75eaa76092f
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-10-30 19:22:20 +0600

    Added Type Casting, Conversion, and Struct

---
+++ Using C#/C#/Struct/Struct/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    struct Point // struct is a value type user-defined data type.
+    {
+        public int x;
+        public int y;
+
+        // programmer can't define his own parameterless constructor in C#
+        //Point()
+        //{
+        //    x = 5;
+        //    y = 5;
+        //}
+
+        public Point(int x, int y) // parameterized constructor
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("({0},{1})", x, y);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+            p.x = 5; p.y = 10;
+            p.Print();
+
+            Point p2 = new Point(8, 10);
+            p2.Print();
+
+            Point p3 = p2;
+            p3.x++;
+            p3.y++;
+            p2.Print(); // p2 will not change because struct is a value type data
+            p3.Print();
+        }
+    }
+}
---
+++ Using C#/C#/TypeCastingAndTypeConversion/TypeCastingAndTypeConversion/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCastingAndTypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // type casting are two types -
+            // 1. Implicit type cast (no possibilities of data loss)
+            // 2. Explicit type cast (there are possibilitites of data loss)
+            int x = 5;
+            long y = x; // implicit type cast
+            short z = (short)x; // explicit type cast
+
+            char c = 'A';
+            int n = c; // implicit type cast
+            byte b = (byte)c; // explicit type cast
+            Console.WriteLine(n); // ascii value of 'A'
+            Console.WriteLine(b); // ascii value of 'A'
+
+            // type conversion - used for conversions that require transformation between fundamentally different types
+
+            // type conversion (string to int) - (string must be fill by numeric char)
+            string s = "545";
+            // x = (int)s; // casting not possible
+            x = Convert.ToInt32(s); // but conversion is possible (will throw an error if conversion is not possible for some reason)
+
+            // int to string
+            x = 1055;
+            s = Convert.ToString(x);
+            Console.WriteLine(s);
+
+            // this is okay
+            y = 1000000;
+            x = Convert.ToInt32(y);
+            Console.WriteLine(x);
+            // but this is not ("y" is greater than "x's" max value, so this will throw an error)
+            y = 10000000000;
+            // x = Convert.ToInt32(y);
+            Console.WriteLine(x);
+        }
+    }
+}
