325011afbb52c3c84ee074834fe261ccd6b63db1
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-11-04 19:39:11 +0600

    Added: Class

---
+++ Using C#/C#/Class/Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // struct vs class
+            // almost similar but there are few minor changes. like-
+            // 1. struct doesn't support inheritance, abstraction
+            // 2. struct doesn't allowed us to create default constructor
+
+            Student s1 = new Student();
+            s1.Print();
+            Student s2 = new Student(1, "Faysal", 3.53);
+            s2.Print();
+        }
+    }
+}
---
+++ Using C#/C#/Class/Class/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    internal class Student
+    {
+        // attributes should be private
+        private int id;
+        private string name;
+        private double cgpa;
+
+        // we should create an empty constructor if we need it and also declare a parameterized constructor
+        public Student() 
+        {
+        }
+
+        // initialize using Setter functions is good practice
+        public Student(int id, string name, double cgpa)
+        {
+            this.SetId(id);
+            this.SetName(name);
+            this.SetCgpa(cgpa);
+        }
+
+
+        // we should use getter and setter function to read/write data
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+
+        public int GetId()
+        {
+            return this.id;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        // we can validate input before set
+        public void SetCgpa(double cgpa)
+        {
+            if (cgpa >= 0 && cgpa <= 4) this.cgpa = cgpa;
+            else this.cgpa = -1;
+        }
+
+        public double GetCgpa() 
+        {
+            return this.cgpa; 
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Student's ID: {0}", this.GetId());
+            Console.WriteLine("Student's Name: {0}", this.GetName());
+            Console.WriteLine("Student's CGPA: {0}", this.GetCgpa());
+        }
+    }
+}
