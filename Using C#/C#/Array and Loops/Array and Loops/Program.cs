21bff6bb8ca91701490efe6112f1648ed6f45d74
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2024-11-11 20:12:44 +0600

    Added: Array and Loops

---
+++ Using C#/C#/Array and Loops/Array and Loops/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_and_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1D array
+            Console.WriteLine("1D Array:");
+            int[] a = new int[5] { 1, 2, 3, 4, 5 };
+            int[] b = a;
+            Console.Write("a: ");
+            foreach (int x in a)
+            {
+                Console.Write("{0} ", x);
+            }
+            Console.WriteLine();
+            Console.Write("b: ");
+            foreach (int x in a)
+            {
+                Console.Write("{0} ", x);
+            }
+            Console.WriteLine();
+
+            b[0] = 100; // changed in b will also change a because array is a reference type data
+
+            Console.Write("a: ");
+            foreach (int x in a)
+            {
+                Console.Write("{0} ", x);
+            }
+            Console.WriteLine();
+            Console.Write("b: ");
+            foreach (int x in a)
+            {
+                Console.Write("{0} ", x);
+            }
+            Console.WriteLine();
+
+            // 2D array
+            Console.WriteLine("\n2D Array:");
+            int[,] arr = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("{0} ", arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            // Jagged Array
+            Console.WriteLine("\nJagged Array:");
+            int[][] jagged = new int[2][];
+            jagged[0] = new int[2] {1, 2};
+            jagged[1] = new int[4] {3, 4, 5, 6};
+            foreach (int[] row in jagged)
+            {
+                foreach (int val in row)
+                {
+                    Console.Write("{0} ", val);
+                }
+                Console.WriteLine();
+            }
+
+
+            // Jagged Array of 2D array
+            int[][,] jaggedOfMatrix = new int[2][,];
+            jaggedOfMatrix[0] = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+            jaggedOfMatrix[1] = new int[2, 3] { {5,6, 7 }, { 8, 9, 10} };
+
+            foreach (int[,] row in jaggedOfMatrix)
+            {
+                // each row is a 2D array
+            }
+        }
+    }
+}
---
+++ Using C#/C#/Array and Loops/Array and Loops/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Array and Loops")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Array and Loops")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ceb6a8f4-6ab7-44b7-8d9c-11553f1935e1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
