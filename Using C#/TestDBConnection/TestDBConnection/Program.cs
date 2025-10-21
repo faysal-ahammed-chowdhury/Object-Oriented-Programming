1dc90ada8c066a9d696cf1fd006c541738772d70
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2025-10-22 01:53:55 +0600

    DB Connection Test

---
+++ Using C#/TestDBConnection/TestDBConnection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TestDBConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection conn = new SqlConnection("Data Source=faysals-pc\\SQLEXPRESS;Initial Catalog=Test;User ID=sa;Password=p@ssword;Encrypt=False;");
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Users", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                Console.WriteLine($"ID: {ds.Tables[0].Rows[i][0].ToString()}");
+                Console.WriteLine($"Name: {ds.Tables[0].Rows[i][1].ToString()}");
+                Console.WriteLine($"Email: {ds.Tables[0].Rows[i][2].ToString()}");
+                Console.WriteLine($"Password: {ds.Tables[0].Rows[i][3].ToString()}\n");
+            }
+        }
+    }
+}
